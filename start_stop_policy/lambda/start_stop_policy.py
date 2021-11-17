14fd8adb5b787fc6e7ef1b93e32d8e018bac5ade
Boxing <boxingp@gmail.com>
2021-11-17 19:00:30 +0800

    SS-3: add a condition to verify whether a weekday.

--- start_stop_policy/lambda/start_stop_policy.py
+++ start_stop_policy/lambda/start_stop_policy.py
@@ -3,7 +3,7 @@ import datetime
 
 def lambda_handler(event, context):
     china_now = datetime.datetime.utcnow() + datetime.timedelta(hours=8)
-    if china_now.time() <= datetime.time(12, 00):
+    if china_now.isoweekday() in range(1, 6) and china_now.time() <= datetime.time(12, 00):
         return True
     else:
         return False
